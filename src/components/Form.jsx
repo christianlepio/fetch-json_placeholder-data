4657a2bb7ffc51b4489dea3418d2b9531aa8c998
christianlepio <rylepio79@gmail.com>
2023-11-01 00:53:52 +0800

    update column for btns

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,21 +4,21 @@ import Button from './Button'
 const Form = ({ reqType, setReqType }) => {
     return (
         <form onSubmit={(e) => e.preventDefault()} className="row justify-content-center border-bottom mb-3">
-            <div className="col-md text-center">
+            <div className="col text-center">
                 <Button 
                     reqType={reqType}
                     setReqType={setReqType}
                     btnName='Fetch Users'
                 />
             </div>
-            <div className="col-md text-center">
+            <div className="col text-center">
                 <Button 
                     reqType={reqType}
                     setReqType={setReqType}
                     btnName='Fetch Posts'
                 />
             </div>
-            <div className="col-md text-center mb-3">
+            <div className="col text-center mb-3">
                 <Button 
                     reqType={reqType}
                     setReqType={setReqType}
