7d529ef43acd4b745551edf4ce33dfda40d63bf6
christianlepio <rylepio79@gmail.com>
2023-11-01 00:40:06 +0800

    add spinners for loading

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ function App() {
   const API_URL = 'https://jsonplaceholder.typicode.com/' //free api url
   const [reqType, setReqType] = useState('users')
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
+    setIsLoading(true)
+
     const fetchItems = async () => {
       try {
         const response = await fetch(`${API_URL}${reqType}`) //fetch api syntax
@@ -17,10 +20,12 @@ function App() {
         setItems(data) //assign fetched data to items state
       } catch (err) {
         console.log(err)
+      } finally {
+        setIsLoading(false)
       }
     }
 
-    fetchItems() //call fetch items funciton
+    setTimeout(() => fetchItems(), 2000) //call fetch items funciton
   }, [reqType])
 
   return (
@@ -33,12 +38,23 @@ function App() {
         />
       </header>
       <main>
-        <div className="container overflow-y-auto rounded-3 border mb-3" style={{maxHeight: '75vh'}}>
-          {/* <List items={items} /> */}
-          <Table 
-            items={items}
-          />
-        </div>
+        {isLoading ? 
+          <div className="d-flex justify-content-center my-5 py-5">
+            <div className="spinner-border my-5" style={{width: '3rem', height: '3rem'}} role="status">
+              <span className="visually-hidden my-5">Loading...</span>
+            </div>&nbsp;&nbsp;
+            <div className="spinner-grow my-5" style={{width: '3rem', height: '3rem'}} role="status">
+              <span className="visually-hidden my-5">Loading...</span>
+            </div>
+          </div>
+        :
+          <div className="container overflow-y-auto rounded-3 border mb-3" style={{maxHeight: '75vh'}}>
+            {/* <List items={items} /> */}
+            <Table 
+              items={items}
+            />
+          </div>
+        }
       </main>
     </>
   )
